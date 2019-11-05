24052efb2c7f8ee0d6e17f6b4de605850e116706
Your Name <you@example.com>
2019-11-05 10:53:50 +0900

    react-native-device-brightness-woogie init commit

---
+++ RNDeviceBrightness/RNDeviceBrightness.h
@@ -0,0 +1,18 @@
+//
+//  RNDeviceBrightness.h
+//  RNDeviceBrightness
+//
+//  Created by Calvin on 3/11/17.
+//  Copyright © 2017 CapsLock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+@interface RNDeviceBrightness : NSObject <RCTBridgeModule>
+
+@end
---
+++ RNDeviceBrightness/RNDeviceBrightness.m
@@ -0,0 +1,39 @@
+//
+//  RNDeviceBrightness.m
+//  RNDeviceBrightness
+//
+//  Created by Calvin on 3/11/17.
+//  Copyright © 2017 CapsLock. All rights reserved.
+//
+
+#import "RNDeviceBrightness.h"
+
+@implementation RNDeviceBrightness
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(setBrightnessLevel:(float)brightnessLevel)
+{
+    /*
+     2019.11.01 edited by woogie.kim
+     Xcode11 iPhone11 이상 계열에서 main thread violation exception이 발생하기 때문에 queue 직렬화로 처리
+     */
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        [UIScreen mainScreen].brightness = brightnessLevel;
+    });
+}
+
+RCT_REMAP_METHOD(getBrightnessLevel,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    /*
+    2019.11.01 edited by woogie.ki
+    Xcode11 iPhone11 이상 계열에서 main thread violation exception이 발생하기 때문에 queue 직렬화로 처리
+    */
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        resolve(@([UIScreen mainScreen].brightness));
+    });
+}
+
+@end
